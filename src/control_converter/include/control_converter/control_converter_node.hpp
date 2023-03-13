e821476939fec45663cffea4851cf0da87c84572
Michael Wu <wuxiaohua1011@gmail.com>
2023-03-12 23:04:23 -0700

    lite: hardware awareness

--- src/control_converter/include/control_converter/control_converter_node.hpp
+++ src/control_converter/include/control_converter/control_converter_node.hpp
@@ -18,9 +18,27 @@ namespace control_converter
         nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state) override;
         nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state) override;
         void onLatestAckermannRcvd(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg);
+        float map_range(float value, float orig_min, float orig_max, float target_min, float target_max)
+        {
+            // calculate the original range
+            float orig_range = orig_max - orig_min;
+
+            // calculate the target range
+            float target_range = target_max - target_min;
+
+            // scale the original value to the target range
+            return (((value - orig_min) * target_range) / orig_range) + target_min;
+        }
+        float ackermann_to_carla_steering(float ackermann_steering);
 
         rclcpp::Publisher<roar_msgs::msg::EgoVehicleControl>::SharedPtr control_publisher_;
         rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr ackermann_sub_;
+
+        float GOKART_MAX_RIGHT_STEERING = 1.0;
+        float GOKART_MAX_LEFT_STEERING = -1.0;
+
+        float INPUT_MAX_RIGHT_STEERING = 1.0;
+        float INPUT_MAX_LEFT_STEERING = 1.0;
     };
 }
 
--- src/control_converter/src/control_converter_node.cpp
+++ src/control_converter/src/control_converter_node.cpp
@@ -7,6 +7,11 @@ namespace control_converter
     ControlConverterNode::ControlConverterNode()
         : LifecycleNode("control_converter_node", "", true)
     {
+        this->declare_parameter("input_max_right_steering", 1.0);
+        this->declare_parameter("input_max_left_steering", -1.0);
+
+        this->INPUT_MAX_RIGHT_STEERING = this->get_parameter("input_max_right_steering").as_double();
+        this->INPUT_MAX_LEFT_STEERING = this->get_parameter("input_max_left_steering").as_double();
         RCLCPP_INFO(get_logger(), "Creating control_converter_node");
     }
 
@@ -50,6 +55,17 @@ namespace control_converter
         ego_vehicle_msg.brake = msg->drive.speed == 0;
         ego_vehicle_msg.reverse = msg->drive.acceleration < 0;
         this->control_publisher_->publish(ego_vehicle_msg);
+    }
+
+    float ControlConverterNode::ackermann_to_carla_steering(float ackermann_steering)
+    {
+        return std::max(GOKART_MAX_LEFT_STEERING,
+                        std::min(GOKART_MAX_RIGHT_STEERING,
+                                 this->map_range(ackermann_steering,
+                                                 GOKART_MAX_LEFT_STEERING,
+                                                 GOKART_MAX_RIGHT_STEERING,
+                                                 INPUT_MAX_LEFT_STEERING,
+                                                 INPUT_MAX_RIGHT_STEERING)));
     }
 }
 
