fe13504f1e8420459bc47a4ddba1e7d274429ee3
Michael Wu <wuxiaohua1011@gmail.com>
2023-07-22 19:43:30 -0700

    lite: lidar num changed

--- src/gokart_hardware_launches/launch/lidar.launch.py
+++ src/gokart_hardware_launches/launch/lidar.launch.py
@@ -9,28 +9,31 @@ from pathlib import Path
 def generate_launch_description():
     base_path = Path(get_package_share_directory("gokart_hardware_launches"))
 
+    user_config_path = base_path / "param" / "center_livox_lidar_config.json"
+
     livox_driver = Node(
         package="livox_ros2_driver",
         executable="livox_ros2_driver_node",
-        name="livox_lidar",
+        name="livox_lidar_publisher",
         output="screen",
         parameters=[
             {
-                "xfer_format": 0  # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
+                "xfer_format": 0 
             },
             {"multi_topic": 1},
             {"data_src": 0},
             {
-                "publish_freq": 10.0
+                "publish_freq": 20.0
             },
             {"output_data_type": 0},
             {
                 "frame_id": "center_lidar",
             },
             {
-                "user_config_path": (base_path / "param" /"center_livox_lidar_config.json").as_posix()
+                "user_config_path": user_config_path.as_posix()
             },
         ],
+        remappings=[("livox/lidar_3JEDK390019Q271", "/roar/front/lidar")]
     )
 
     return LaunchDescription(
